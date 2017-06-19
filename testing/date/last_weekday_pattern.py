1faaa1ff26bb341ed026386228d9ba9b8249e395
Eryk Humberto Oliveira Alves <erykhumberto@hotmail.com>
2017-06-19 03:42:31 -0300

    tdd_pyunit2 not finished yet. I'm at: http://www.onlamp.com/pub/a/python/2005/02/03/tdd_pyunit2.html?page=3

---
+++ testing/date/last_weekday_pattern.py
@@ -0,0 +1,10 @@
+import datetime
+
+
+class LastWeekdayPattern(object):
+    def __init__(self, weekday):
+        self.weekday = weekday
+
+    def matches(self, date):
+        next_week = date + datetime.timedelta(7)
+        return self.weekday == date.weekday() and next_week.month != date.month
--- testing/tests/date_pattern/test_date_pattern.py
+++ testing/tests/date_pattern/test_date_pattern.py
@@ -4,35 +4,84 @@
 import datetime
 import unittest
 
-from date.date_pattern import DatePattern
+from date.composite_pattern import CompositePattern
+from date.day_pattern import DayPattern
+from date.month_pattern import MonthPattern
+from date.weekday_pattern import WeekdayPattern
+from date.year_pattern import YearPattern
+from date.last_weekday_pattern import LastWeekdayPattern
+
+MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY = range(0, 7)
 
 
 class DatePatternTests(unittest.TestCase):
-    def test_matches_true(self):
-        actual = DatePattern(2017, 10, 10)
-        expected = datetime.date(2017, 10, 10)
-        self.assertTrue(actual.matches(expected))
+    def setUp(self):
+        self.date = datetime.date(2004, 9, 29)
 
-    def test_matches_false(self):
-        actual = DatePattern(2017, 10, 10)
-        expected = datetime.date(2017, 10, 11)
-        self.assertFalse(actual.matches(expected))
+    def test_year_pattern_matches(self):
+        year_pattern = YearPattern(2004)
+        self.assertTrue(year_pattern.matches(self.date))
 
-    def test_matches_year_as_wild_card(self):
-        actual = DatePattern(0, 10, 10)
-        expected = datetime.date(2017, 10, 10)
-        self.assertTrue(actual.matches(expected))
+    def test_year_pattern_does_not_match(self):
+        year_pattern = YearPattern(2005)
+        self.assertFalse(year_pattern.matches(self.date))
 
-    def test_matches_month_as_wild_card(self):
-        actual = DatePattern(0, 0, 10)
-        expected = datetime.date(2017, 10, 10)
-        self.assertTrue(actual.matches(expected))
+    def test_month_pattern_matches(self):
+        month_pattern = MonthPattern(9)
+        self.assertTrue(month_pattern.matches(self.date))
 
-    def test_matches_weekday(self):
-        actual = DatePattern(0, 0, 0, 2) # 2 is Weekday
-        expected = datetime.date(2004, 9, 29)
-        self.assertTrue(actual.matches(expected))
+    def test_month_pattern_does_not_match(self):
+        month_pattern = MonthPattern(11)
+        self.assertFalse(month_pattern.matches(self.date))
+
+    def test_day_pattern_matches(self):
+        day_pattern = DayPattern(29)
+        self.assertTrue(day_pattern.matches(self.date))
+
+    def test_day_pattern_does_not_match(self):
+        day_pattern = DayPattern(21)
+        self.assertFalse(day_pattern.matches(self.date))
+
+    def test_weekday_pattern_matches(self):
+        weekday_pattern = WeekdayPattern(2)  # Wednesday
+        self.assertTrue(weekday_pattern.matches(self.date))
+
+    def test_weekday_pattern_does_not_match(self):
+        weekday_pattern = WeekdayPattern(1)  # Tuesday
+        self.assertFalse(weekday_pattern.matches(self.date))
+
+    def test_composite_matches(self):
+        cp = CompositePattern()
+        cp.add(YearPattern(2004))
+        cp.add(MonthPattern(9))
+        cp.add(DayPattern(29))
+        self.assertTrue(cp.matches(self.date))
+
+    def test_composite_does_not_match(self):
+        cp = CompositePattern()
+        cp.add(YearPattern(2004))
+        cp.add(MonthPattern(9))
+        cp.add(DayPattern(30))
+        self.assertFalse(cp.matches(self.date))
+
+    def test_composite_without_year_matches(self):
+        cp = CompositePattern()
+        cp.add(MonthPattern(9))
+        cp.add(DayPattern(29))
+        self.assertTrue(cp.matches(self.date))
 
 
-if __name__ == '__main__':
-    unittest.main()
+class LastWeekdayPatternTests(unittest.TestCase):
+    def setUp(self):
+        self.pattern = LastWeekdayPattern(WEDNESDAY)
+
+    def test_last_wednesday_matches(self):
+        last_wed_of_sept_2004 = datetime.date(2004, 9, 29)
+        self.assertTrue(self.pattern.matches(last_wed_of_sept_2004))
+
+    def test_last_wednesday_does_not_match(self):
+        first_wed_of_sept_2004 = datetime.date(2004, 9, 1)
+        self.assertFalse(self.pattern.matches(first_wed_of_sept_2004))
+
+
+if __name__ == '__main__':   unittest.main()
